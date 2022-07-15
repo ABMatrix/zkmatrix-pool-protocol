e15c5ff2708385785221288f310cec607bdfa3ae
IronC <apehole@163.com>
2022-07-15 14:34:46 +0800

    add ABMatrixPoolMiner ABMatrixPoolAgent

--- environment/src/lib.rs
+++ environment/src/lib.rs
@@ -1,8 +1,47 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use std::marker::PhantomData;
+use snarkvm::dpc::Network;
+use snarkos::environment::{Environment, helpers::NodeType};
+
+#[derive(Clone, Debug, Default)]
+pub struct ABMatrixPoolMiner<N: Network>(PhantomData<N>);
+
+#[rustfmt::skip]
+impl<N: Network> Environment for ABMatrixPoolMiner<N> {
+    type Network = N;
+    const NODE_TYPE: NodeType = NodeType::Miner;
+    const MINIMUM_NUMBER_OF_PEERS: usize = 0;
+    const MAXIMUM_NUMBER_OF_PEERS: usize = 0;
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct ABMatrixPoolAgent<N: Network>(PhantomData<N>);
+
+#[rustfmt::skip]
+impl<N: Network> Environment for ABMatrixPoolAgent<N> {
+    type Network = N;
+    const NODE_TYPE: NodeType = NodeType::Miner;
+    const MINIMUM_NUMBER_OF_PEERS: usize = 0;
+    const MAXIMUM_NUMBER_OF_PEERS: usize = 0;
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct ABMatrixPoolMinerTrial<N: Network>(PhantomData<N>);
+
+#[rustfmt::skip]
+impl<N: Network> Environment for ABMatrixPoolMinerTrial<N> {
+    type Network = N;
+    const NODE_TYPE: NodeType = NodeType::Miner;
+    const MINIMUM_NUMBER_OF_PEERS: usize = 0;
+    const MAXIMUM_NUMBER_OF_PEERS: usize = 0;
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct ABMatrixPoolAgentTrial<N: Network>(PhantomData<N>);
+
+#[rustfmt::skip]
+impl<N: Network> Environment for ABMatrixPoolAgentTrial<N> {
+    type Network = N;
+    const NODE_TYPE: NodeType = NodeType::Miner;
+    const MINIMUM_NUMBER_OF_PEERS: usize = 0;
+    const MAXIMUM_NUMBER_OF_PEERS: usize = 0;
 }
