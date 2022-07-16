713af3e1bf120bc57677854329a2a82cff645b64
IronC <apehole@163.com>
2022-07-17 00:00:08 +0800

    add MIN_SUPPORTED_PROTOCOL_VERSION  MAX_SUPPORTED_PROTOCOL_VERSION

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use semver::Version;
+
 pub mod environment;
 pub mod message;
 pub mod speedometer;
 
 pub static PROTOCOL_PREFIX: &'static str = "ABMatrix";
+pub static MIN_SUPPORTED_PROTOCOL_VERSION: Version = Version::new(0, 1, 0);
+pub static MAX_SUPPORTED_PROTOCOL_VERSION: Version = Version::new(0, 1, 0);
