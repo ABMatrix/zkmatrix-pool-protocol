5b18d64dded48dacb5ca083c96775ecba31128e1
IronC <apehole@163.com>
2022-07-18 22:50:19 +0800

    add PoolNetwork

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use semver::Version;
+use snarkvm::dpc::testnet2::Testnet2;
 
 pub mod environment;
 pub mod message;
@@ -7,3 +8,4 @@ pub mod speedometer;
 pub static PROTOCOL_PREFIX: &'static str = "ABMatrix";
 pub static MIN_SUPPORTED_PROTOCOL_VERSION: Version = Version::new(0, 1, 0);
 pub static MAX_SUPPORTED_PROTOCOL_VERSION: Version = Version::new(0, 1, 0);
+pub type PoolNetwork = Testnet2;
