c7568f102a4092699420ca803fd425985117c628
IronC <apehole@163.com>
2022-07-18 11:23:12 +0800

    fmt

--- src/message/stratum.rs
+++ src/message/stratum.rs
@@ -1,7 +1,8 @@
 use std::io;
 use bytes::BytesMut;
-use json_rpc_types::{Error, ErrorCode, Id, Request, Response, Version};
-use json_rpc_types::Id::Num;
+use json_rpc_types::{Error, Id, Request, Response, Version};
+#[cfg(test)]
+use json_rpc_types::ErrorCode;
 use serde_json::Value;
 use tokio_util::codec::{AnyDelimiterCodec, Decoder, Encoder};
 use super::response::ResponseMessage;
@@ -281,7 +282,7 @@ impl Decoder for StratumCodec {
 fn test_encode_decode() {
     let mut codec = StratumCodec::default();
     //Subscribe
-    let msg = StratumMessage::Subscribe(Num(0), "ABMatrix_Aleo_Miner".to_string(), "ABMatrix_Aleo_Miner_4".to_string(), Some("session".to_string()));
+    let msg = StratumMessage::Subscribe(Id::Num(0), "ABMatrix_Aleo_Miner".to_string(), "ABMatrix_Aleo_Miner_4".to_string(), Some("session".to_string()));
     let mut buf1 = BytesMut::new();
     codec.encode(msg, &mut buf1).unwrap();
     let res = codec.decode(&mut buf1.clone()).unwrap().unwrap();
@@ -291,7 +292,7 @@ fn test_encode_decode() {
 
 
     //Authorize
-    let msg = StratumMessage::Authorize(Num(0), "account_name".to_string(), "worker_name".to_string(), None);
+    let msg = StratumMessage::Authorize(Id::Num(0), "account_name".to_string(), "worker_name".to_string(), None);
     let mut buf1 = BytesMut::new();
     codec.encode(msg, &mut buf1).unwrap();
     let res = codec.decode(&mut buf1.clone()).unwrap().unwrap();
@@ -326,7 +327,7 @@ fn test_encode_decode() {
     assert_eq!(buf1, buf2);
 
     // Submit
-    let msg = StratumMessage::Submit(Num(2),"job_id".to_string(), "nonce".to_string(), "proof".to_string());
+    let msg = StratumMessage::Submit(Id::Num(0),"job_id".to_string(), "nonce".to_string(), "proof".to_string());
     let mut buf1 = BytesMut::new();
     codec.encode(msg, &mut buf1).unwrap();
     let res = codec.decode(&mut buf1.clone()).unwrap().unwrap();
@@ -335,7 +336,7 @@ fn test_encode_decode() {
     assert_eq!(buf1, buf2);
 
     // Response(Id, Option<ResponseMessage>, Option<Error<()>>),
-    let msg = StratumMessage::Response(Num(3), None, Some(Error::with_custom_msg(ErrorCode::InvalidParams, "test error")));
+    let msg = StratumMessage::Response(Id::Num(0), None, Some(Error::with_custom_msg(ErrorCode::InvalidParams, "test error")));
     let mut buf1 = BytesMut::new();
     codec.encode(msg, &mut buf1).unwrap();
     let res = codec.decode(&mut buf1.clone()).unwrap().unwrap();
